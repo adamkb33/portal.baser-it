b8f904716d2f338a6e9dea710eaa418c1214c4b8
adamkb33 <adamkbaser@gmail.com>
2025-12-11 23:03:07 +0100

    done

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,4 @@ import { type RouteConfig, index } from '@react-router/dev/routes';
 import { ROUTE_TREE } from './lib/route-tree';
 import { buildRoutesNested } from './lib/routes-builder';
 
-console.log(JSON.stringify(buildRoutesNested(ROUTE_TREE)));
-
 export default [index('./routes/home.tsx'), ...buildRoutesNested(ROUTE_TREE)] satisfies RouteConfig;
